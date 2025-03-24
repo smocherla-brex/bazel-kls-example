f5e90a9c961e2ea186b3f8b384e345962cfb5547
Sridhar Mocherla <smocherla@brex.com>
2025-03-24 18:09:57 -0400

    Add proto example

--- src/main/kotlin/com/foo/helloworld/Foo.kt
+++ src/main/kotlin/com/foo/helloworld/Foo.kt
@@ -1,10 +1,13 @@
 package com.foo.helloworld
 
 import jakarta.inject.Inject
+import foo.helloworld.proto.Foo
 
 class Foo(@Inject val name: String) {
 
     fun hello() {
         println("hello ${name}")
+        val foo = Foo(name)
+        println(foo.hello())
     }
 }
